17c358942e24f010e293a59b310e42c7a0cad95d
Krister S. Moen <krister.sig.moen@live.no>
2018-08-31 12:23:24 +0200

    Auth0 change

--- chesterfield-backend-react/src/Service/Auth.js
+++ chesterfield-backend-react/src/Service/Auth.js
@@ -5,7 +5,7 @@ export default class Auth {
   auth0 = new auth0.WebAuth({
     domain: 'chesterfield.eu.auth0.com',
     clientID: '8edEJiiW1q9LuTFbJ-ZDmt09hdydUOHu',
-    redirectUri: 'chesterfield-form.herokuapp.com/home',
+    redirectUri: 'https://chesterfield-form.herokuapp.com/home',
     audience: 'https://chesterfield.eu.auth0.com/userinfo',
     responseType: 'token id_token',
     scope: 'openid'
